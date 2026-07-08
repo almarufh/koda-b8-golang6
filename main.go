fa61b7f196aa4034a15975bbe5d2b211a387c240
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-09 01:39:36 +0800

    fix: receive channel

--- main.go
+++ main.go
@@ -2,66 +2,38 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"sync"
+	"goroutine-control/utils"
 	"time"
 )
 
-func order(wait int, proces string, orderSucces *[]string, wg *sync.WaitGroup) {
-	timeWait := strconv.Itoa(wait)
-	time.Sleep(2 * time.Second)
-	*orderSucces = append(*orderSucces, proces+" dalam "+timeWait+" Detik")
-	defer wg.Done()
-}
-
 func main() {
-	wg := sync.WaitGroup{}
-	orderSucces := []string{}
-
-	type request struct {
-		urut    sync.Mutex
-		request string
-		time    int
-	}
-	askOrder := []request{
-		{
-			urut:    1,
-			request: "Nasi Goreng",
-			time:    5,
-		},
-		{
-			urut:    2,
-			request: "Mie Goreng",
-			time:    2,
-		},
-		{
-			urut:    3,
-			request: "Kopi Luak",
-			time:    1,
-		},
-		{
-			urut:    4,
-			request: "Jus Apel",
-			time:    1,
-		},
-		{
-			urut:    5,
-			request: "Nasi Mandhi",
-			time:    3,
-		},
+	addCounter := 0
+	for x := range utils.BookOrders {
+		request := utils.BookOrders[x].Request
+		time := utils.BookOrders[x].Time
+		urut := utils.BookOrders[x].Urut
+		utils.Wait.Add(1)
+		go utils.Worker(urut, time, request)
+		addCounter++
 	}
 
-	for x := range askOrder {
-		wg.Add(1)
-		request := askOrder[x].request
-		time := askOrder[x].time
-		fmt.Printf("Proses (%d) [%s]\n", x+1, request)
-		go order(time, request, &orderSucces, &wg)
+	if addCounter == len(utils.BookOrders) {
+		time.Sleep(time.Duration(1) * time.Second)
+		fmt.Printf("\n\n")
+		utils.Counter = 1
 	}
+	defer utils.Wait.Wait()
 
-	wg.Wait()
+	fmt.Println(<-utils.OrderSucces)
+	fmt.Println(<-utils.OrderSucces)
+	fmt.Println(<-utils.OrderSucces)
+	fmt.Println(<-utils.OrderSucces)
+	fmt.Println(<-utils.OrderSucces)
 
-	for i := range orderSucces {
-		fmt.Printf("%d. %s\n", i+1, orderSucces[i])
+	fmt.Printf("\nMelihat urutan daftar pesanan selesai...\n\n")
+	time.Sleep(2 * time.Millisecond)
+
+	for x := range utils.OrderSuccesSlice {
+		fmt.Printf("%d. %s\n", x, utils.OrderSuccesSlice[x])
 	}
 }
